424ca76529279f50111d051431a49f55dfcbd661
monkeontheroof <khangkhoailang20@gmail.com>
2024-05-07 18:24:08 +0700

    system.properties

--- src/main/java/com/example/springcoffeeshop/util/security/resource/AuthenticaionResource.java
+++ src/main/java/com/example/springcoffeeshop/util/security/resource/AuthenticaionResource.java
@@ -7,12 +7,10 @@ import com.example.springcoffeeshop.util.security.service.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/v1/auth")
 public class AuthenticaionResource {
 
